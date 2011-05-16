3234904061f951ed912d82d5b775b8df2005c63a
Nick Pack <nick@nickpack.com>
2011-05-16 19:12:11 +0100

    Initial Commit

---
+++ Bootlace/BootlaceAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  BootlaceAppDelegate.h
+//  Bootlace
+//
+//  Created by Nick on 16/05/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BootlaceAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
