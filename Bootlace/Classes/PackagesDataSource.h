b1a6ef94c95dfa3c7e22dd68648bcf168160a626
Nick Pack <nick@nickpack.com>
2011-05-17 20:54:00 +0100

    Center Button initial graphic (steel oib logo)

---
+++ Bootlace/Classes/PackagesDataSource.h
@@ -0,0 +1,16 @@
+//
+//  PackagesDataSource.h
+//  Bootlace
+//
+//  Created by Nick on 17/05/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PackagesDataSource : TTSectionedDataSource {
+    
+}
+
+@end
---
+++ Bootlace/Classes/PackagesDataSource.m
@@ -0,0 +1,14 @@
+//
+//  PackagesDataSource.m
+//  Bootlace
+//
+//  Created by Nick on 17/05/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PackagesDataSource.h"
+
+
+@implementation PackagesDataSource
+
+@end
